d41261508642d6b83fb5eb4a803dc5e6a6e600b0
niels789 <nielsdegroot@live.be>
2021-07-07 23:05:14 +0200

    install prettier and eslint

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,4 +1,4 @@
-import Todo from "../models/todoModel.js";
+// import Todo from "../models/todoModel.js";
 import asyncHandler from "express-async-handler";
 
 const addTodo = asyncHandler(async (req, res) => {
--- frontend/src/components/AddEditTodoForm.jsx
+++ frontend/src/components/AddEditTodoForm.jsx
@@ -1,45 +1,35 @@
-import React, { useState } from "react";
-import PctInput from "./PctInput";
-import PctTextBox from "./PctTextBox";
-import PctDate from "./PctDate";
+import React, { useState } from 'react'
+import PctInput from './PctInput'
+import PctTextBox from './PctTextBox'
+import PctDate from './PctDate'
 
-import axios from "axios";
+import axios from 'axios'
 
 const AddEditTodoForm = () => {
-  const [todo, setTodo] = useState({});
+  const [todo, setTodo] = useState({})
 
   const submitTodo = () => {
-    axios.post("http://localhost:5000/todos/add", todo);
-  };
+    axios.post('http://localhost:5000/todos/add', todo)
+  }
 
   const onChange = (name, value) => {
-    setTodo({ ...todo, [name]: value });
-  };
+    setTodo({ ...todo, [name]: value })
+  }
 
   return (
     <div className="addForm">
       <label for="name">Name:</label>
-      <PctInput
-        name="name"
-        label="Todo name"
-        value={todo?.name}
-        onChange={onChange}
-      />
+      <PctInput name="name" label="Todo name" value={todo?.name} onChange={onChange} />
       <PctTextBox
         name="description"
         label="Description"
         value={todo?.description}
         onChange={onChange}
       />
-      <PctDate
-        name="date"
-        label="Date"
-        value={todo?.date}
-        onChange={onChange}
-      />
+      <PctDate name="date" label="Date" value={todo?.date} onChange={onChange} />
       <button onClick={submitTodo}>Add todo</button>
     </div>
-  );
-};
+  )
+}
 
-export default AddEditTodoForm;
+export default AddEditTodoForm
