d9a19768fa1adefd20dd6065c597bec26713c36a
niels789 <nielsdegroot@live.be>
2021-07-08 23:59:31 +0200

    fetch todos

--- frontend/src/components/TodoPage.jsx
+++ frontend/src/components/TodoPage.jsx
@@ -1,31 +1,32 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import AddEditTodoForm from './AddEditTodoForm'
 import SearchBar from '../components/SearchBar'
-import axios from 'axios'
+import Todo from '../components/Todo'
+import useAxios from '../hooks/useAxios'
 
 const TodoPage = () => {
   const [filterText, setFilterText] = useState()
   const [todos, setTodos] = useState()
+  // TODO: spinner
+  const { response, loading, execute } = useAxios({
+    method: 'get',
+    url: 'todos/getTodos',
+  })
 
-  const getTodos = async () => {
-    const todos = await axios.get('todos/getTodos')
-    if (todos?.data) {
-      setTodos(todos.data)
+  useEffect(() => {
+    if (response) {
+      setTodos(response)
     }
-  }
-
-  if (!todos) {
-    getTodos()
-  }
+  }, [response])
 
   return (
-    <div id="todoPage">
+    <div class="page-layout">
       <h1>Todos</h1>
       <SearchBar setFilterText={e => setFilterText(e)} />
       <AddEditTodoForm />
-      {/* {todos.map((todo) => (
-        <h2>{todo}</h2>
-      ))} */}
+      <div id="todo-container">
+        {loading ? <p>Loading...</p> : todos.map(todo => <Todo todo={todo} />)}
+      </div>
     </div>
   )
 }
