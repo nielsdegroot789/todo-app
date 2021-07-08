1858a2f4c8d1219b97564ca090c841ab2f87272c
niels789 <nielsdegroot@live.be>
2021-07-09 00:00:02 +0200

    routes delete and update

--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -1,8 +1,10 @@
-import { addTodo, getTodos } from '../controllers/todoController.js'
+import { addTodo, getTodos, deleteTodo, updateTodo } from '../controllers/todoController.js'
 import express from 'express'
 const router = express.Router()
 
 router.route('/getTodos').get(getTodos)
 router.route('/add').post(addTodo)
+router.route('/delete').post(deleteTodo)
+router.route('/update').post(updateTodo)
 
 export default router
