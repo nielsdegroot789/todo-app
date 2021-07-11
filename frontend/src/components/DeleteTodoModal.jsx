c96793d19e7a58262f48b9e4935b1a68dc292a1d
niels789 <nielsdegroot@live.be>
2021-07-11 19:56:04 +0200

    delete todo modal

---
+++ frontend/src/components/DeleteTodoModal.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import useAxios from '../hooks/useAxios'
+import PctModal from './PctModal'
+
+const DeleteTodoModal = ({ isVisible, onClose, todoId, refresh }) => {
+  const { execute } = useAxios({
+    axiosConfig: {
+      method: 'delete',
+      url: 'todos/delete',
+      data: { _id: todoId },
+    },
+    successMessage: 'Todo successfully deleted',
+    successFunction: refresh,
+    manualCancel: true,
+  })
+
+  return (
+    <PctModal
+      title="Delete todo"
+      isVisible={isVisible}
+      onClose={onClose}
+      extra={<button onClick={execute}>Delete todo</button>}
+    >
+      <p>Are you sure you want to delete this todo?</p>
+    </PctModal>
+  )
+}
+
+export default DeleteTodoModal
