dfd5692e32b1dbc56862563a58b72b90e133cf3f
niels789 <nielsdegroot@live.be>
2021-07-11 19:56:49 +0200

    simplify todo

--- frontend/src/components/Todo.jsx
+++ frontend/src/components/Todo.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import useToggle from '../hooks/useToggle'
 
-const Todo = ({ todo, deleteTodo }) => {
+const Todo = ({ todo, setTodoId }) => {
   return (
     <div id="todo-item-container">
       <h4>{todo?.name}</h4>
       <h4>{todo?.description}</h4>
       <h4>{todo?.date}</h4>
-      <button onClick={deleteTodo}>Delete todo</button>
+      <button onClick={() => setTodoId(todo._id)}>delete todo</button>
     </div>
   )
 }
