88eb5fab5566308b07e199fac5b102029008b751
niels789 <nielsdegroot@live.be>
2021-08-15 15:41:16 +0200

    install gpx converter

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef } from 'react'
 import mapboxgl from '!mapbox-gl' // eslint-disable-line import/no-webpack-loader-syntax
 // TODO: put in.env file
 mapboxgl.accessToken =
@@ -31,6 +31,52 @@ const Map = () => {
           'sky-type': 'atmosphere',
           'sky-atmosphere-sun': [0.0, 0.0],
           'sky-atmosphere-sun-intensity': 15,
+        },
+      })
+      map.addSource('route', {
+        type: 'geojson',
+        data: {
+          type: 'Feature',
+          properties: {},
+          geometry: {
+            type: 'LineString',
+            coordinates: [
+              [-122.483696, 37.833818],
+              [-122.483482, 37.833174],
+              [-122.483396, 37.8327],
+              [-122.483568, 37.832056],
+              [-122.48404, 37.831141],
+              [-122.48404, 37.830497],
+              [-122.483482, 37.82992],
+              [-122.483568, 37.829548],
+              [-122.48507, 37.829446],
+              [-122.4861, 37.828802],
+              [-122.486958, 37.82931],
+              [-122.487001, 37.830802],
+              [-122.487516, 37.831683],
+              [-122.488031, 37.832158],
+              [-122.488889, 37.832971],
+              [-122.489876, 37.832632],
+              [-122.490434, 37.832937],
+              [-122.49125, 37.832429],
+              [-122.491636, 37.832564],
+              [-122.492237, 37.833378],
+              [-122.493782, 37.833683],
+            ],
+          },
+        },
+      })
+      map.addLayer({
+        id: 'route',
+        type: 'line',
+        source: 'route',
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round',
+        },
+        paint: {
+          'line-color': '#888',
+          'line-width': 8,
         },
       })
     })
