ffdb5344a64ed4ff6964c91534828297b474eb11
niels789 <nielsdegroot@live.be>
2021-08-15 15:17:53 +0200

    add 3d to map

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,36 +1,61 @@
 import React, { useEffect, useRef, useState } from 'react'
 import mapboxgl from '!mapbox-gl' // eslint-disable-line import/no-webpack-loader-syntax
-
+// TODO: put in.env file
 mapboxgl.accessToken =
   'pk.eyJ1IjoibmllbHM3ODkiLCJhIjoiY2txanRlMHd3MDJpZDJ2cG1kNzY2dHNqaCJ9.LQ9eDZh0I7ZNntJrAR52tg'
 
 const Map = () => {
   const mapContainer = useRef(null)
-  const map = useRef(null)
-  const [lng, setLng] = useState(-70.9)
-  const [lat, setLat] = useState(42.35)
-  const [zoom, setZoom] = useState(9)
-
   useEffect(() => {
-    if (map.current) return
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      zoom: zoom,
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/satellite-streets-v11',
+      center: [-119.99959421984575, 38.619551620333496],
+      zoom: 14,
     })
-  })
+    map.on('load', () => {
+      map.addSource('mapbox-dem', {
+        type: 'raster-dem',
+        url: 'mapbox://mapbox.mapbox-terrain-dem-v1',
+        tileSize: 512,
+        maxzoom: 14,
+      })
+      // add the DEM source as a terrain layer with exaggerated height
+      map.setTerrain({ source: 'mapbox-dem', exaggeration: 1.5 })
 
-  useEffect(() => {
-    if (!map.current) return
-    map.current.on('move', () => {
-      setLng(map.current.getCenter().lng.toFixed(4))
-      setLat(map.current.getCenter().lat.toFixed(4))
-      setZoom(map.current.getZoom().toFixed(2))
+      // add a sky layer that will show when the map is highly pitched
+      map.addLayer({
+        id: 'sky',
+        type: 'sky',
+        paint: {
+          'sky-type': 'atmosphere',
+          'sky-atmosphere-sun': [0.0, 0.0],
+          'sky-atmosphere-sun-intensity': 15,
+        },
+      })
     })
-  })
+  }, [])
 
-  return <div ref={mapContainer} className="map-container" />
+  // useEffect(() => {
+  //   if (map.current) return
+  //   map.current = new mapboxgl.Map({
+  //     container: mapContainer.current,
+  //     style: 'mapbox://styles/mapbox/streets-v11',
+  //     center: [lng, lat],
+  //     zoom: zoom,
+  //   })
+  // })
+
+  // useEffect(() => {
+  //   if (!map.current) return
+  //   map.current.on('move', () => {
+  //     setLng(map.current.getCenter().lng.toFixed(4))
+  //     setLat(map.current.getCenter().lat.toFixed(4))
+  //     setZoom(map.current.getZoom().toFixed(2))
+  //   })
+  // })
+
+  return <div ref={mapContainer} id="map" className="map-container" />
 }
 
 export default Map
