b3299604026e93146a86b6a6fad3adefc0c79041
Niels De Groot <nielsdegroot@live.be>
2021-08-22 23:09:11 +0200

    delete comment

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    //database Name
     const databaseName = "pct-map";
     const con = await mongoose.connect(
       `mongodb://127.0.0.1:27017/${databaseName}`,
