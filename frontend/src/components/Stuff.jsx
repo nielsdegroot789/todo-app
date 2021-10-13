9890f623e09b84b884215ce30b7bf4ad88a1d87e
Niels De Groot <nielsdegroot@live.be>
2021-10-13 23:17:06 +0200

    cleanup stuff presentation

--- frontend/src/components/Stuff.jsx
+++ frontend/src/components/Stuff.jsx
@@ -3,8 +3,16 @@ import React from 'react'
 const Stuff = ({ stuff, setEditStuff, setDeleteId, setActionable }) => {
   return (
     <div id="stuff-item-container">
-      <h4>{stuff?.name}</h4>
-      <h4>{stuff?.description}</h4>
+      <div className="flex--space-center">
+        <h4>Name: </h4>
+        &nbsp;
+        <h4>{stuff?.name}</h4>
+      </div>
+      <div className="flex--space-center">
+        <h4>More info: </h4>
+        &nbsp;
+        <h4>{stuff?.description}</h4>
+      </div>
       <h4>{stuff?.date}</h4>
       <button onClick={() => setActionable(stuff._id)}>handle stuff</button>
       <button onClick={() => setDeleteId(stuff._id)}>Delete stuff</button>
