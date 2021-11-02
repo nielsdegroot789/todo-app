57d0b1d9c23f0ae37272d8881d50df3639a3c9d4
Niels De Groot <nielsdegroot@live.be>
2021-11-02 23:14:26 +0100

    made PctAddModal

--- frontend/src/components/PctAddModal.jsx
+++ frontend/src/components/PctAddModal.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import useAdd from '../hooks/useAdd'
 import PctModal from './PctModal'
 
-const PctAddModal = ({ config }) => {
+const PctAddModal = ({ title, visible, onClose, collection, form, addData }) => {
+  const { executeAdd } = useAdd({
+    addData,
+    collection,
+    title,
+    successFunction: onClose,
+  })
+
   return (
     <div>
-      <PctModal title={`add ${name}`} visible={isToggled} onClose={toggle}></PctModal>
+      <PctModal title={`add ${title}`} visible={visible} onClose={onClose} onSubmit={executeAdd}>
+        {form}
+      </PctModal>
     </div>
   )
 }
